d77d9bc943bfb6bbfa4d876c904e1293581464a3
Jonathan Rainer <jonrainer@googlemail.com>
2016-01-22 12:18:59 +0000

    "Starting from scratch with the equilibrium finder, not quite sure how one implements something like I'm envisioning in MatLab so it'll be a voyage of discovery."

--- equilibria_finder.m
+++ equilibria_finder.m
@@ -1,13 +1,17 @@
 function equilibria = equilibria_finder(iterations)
-%EQUILIBRIA_FINDER Summary of this function goes here
-%   Detailed explanation goes here
+%EQUILIBRIA_FINDER Will find the fixed points of a given dynamical system.
+%   Runs fsolve over a number of iterations to try and discover as 
+%   many points of the dynamical system as possible. 
+
+    % Set up a structure to store the output equilibrium
     equilibria = zeros(iterations, 2);
-    for i=1:iterations
-        options = optimset('Display','Off');
-        [eq_temp,~,exit,~] = fsolve(@lvderivs, rand(1,2), options);
-        if (exit >= 0) && ismember(eq_temp, equilibria, 'rows') == 0
-            equilibria(i,:) = eq_temp;
-        end
-    end
+    
+%     for i=1:iterations
+%         options = optimset('Display','Off');
+%         [eq_temp,~,exit,~] = fsolve(@lvderivs, rand(1,2), options);
+%         if (exit >= 0) && ismember(eq_temp, equilibria, 'rows') == 0
+%             equilibria(i,:) = eq_temp;
+%         end
+%     end
 end
 
