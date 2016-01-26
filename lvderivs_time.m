d118078f4aee6f6b57b8fae999ace9c5dfec673a
Jonathan Rainer <jonrainer@googlemail.com>
2016-01-26 21:53:40 +0000

    "Sorted out lvderivs_time so it's just a wrapper for lvderivs rather than a seperate function that duplicates global variables."

--- lvderivs_time.m
+++ lvderivs_time.m
@@ -1,21 +1,6 @@
 function F = lvderivs_time(t, co_ords)
 %LVDERIVS_TIME Calculate a vector of the derivate of the two functions in 
 %our Lotka-Volterra Dynamical System.
-    % Constants
-    global a b c d
-    a = 1.0;
-    b = 0.75;
-    c = 1.6;
-    d = 0.4;
-   
-    % Recurring parts 
-    xy = co_ords(1)*co_ords(2);
-    
-    % Calculate derivatives
-    dxdt = a*co_ords(1) - b*xy;
-    dydt = c*xy - d*co_ords(2);
-    
-    % Arrange derivatives into a vector
-    F = [dxdt; dydt];
+    F = lvderivs(co_ords);
 end
 
