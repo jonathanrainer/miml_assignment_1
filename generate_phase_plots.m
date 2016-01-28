837c7cdad27384e5de3e9d33485f06efb9356103
Jonathan Rainer <jonrainer@googlemail.com>
2016-01-28 14:53:15 +0000

    Removing some redundant code that came from the conception of the generation scripts.

--- generate_phase_plots.m
+++ generate_phase_plots.m
@@ -1,15 +1,7 @@
 clf
-suptitle(sprintf('ODE45 Plots for d/c at various starting values\n\n'));
-for p = 1:3
-    start_val = rand_range(1,3,1);
-    for k = 1:3
-        subplot(3,3,(p-1)*3 + k)
-        [t,y] = ode45(@lvderivs_time, 0:0.1:k*50, [d/c,start_val]);
-        plot(t,y)
-        legend({'x(t)','y(t)'},'FontSize', 16);
-        xlabel('Time', 'FontSize', 16);
-        ylabel('Solution Variable', 'FontSize', 16);
-        title(sprintf('t = %d, ystart = %d', k*50, start_val), ...
-            'FontSize', 16);
-    end
+for k = 1:3
+    subplot(1,3, k)
+    phase_portrait_trajectories(@lvderivs_time, [0,4],[0,4],30, ...
+        d/c, 0:0.5:3 ,k*50)
+    title(sprintf('t = %d', k*50), 'FontSize', 16);
 end
